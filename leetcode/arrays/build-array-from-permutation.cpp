0f7cf1d53086b259fca1a791b4f5eb6567747257
Aditya Bilgundi <adiityabilgundi5231@gmail.com>
2021-12-06 00:20:19 +0530

    added leetcode problems

---
+++ leetcode/arrays/build-array-from-permutation.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> arr;
+        int i = 0;
+        while(nums.size() != arr.size()){
+            arr.push_back(nums[nums[i]]);
+            i++;
+        }
+        return arr;
+    }
+};
